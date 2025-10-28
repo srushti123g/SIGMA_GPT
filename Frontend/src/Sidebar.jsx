716c39cfde020cebd17d4485db1e476b0012a579
srushti123g <146696632+srushti123g@users.noreply.github.com>
2025-10-28 21:23:35 +0530

    Update logo image source in Sidebar component

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -2,6 +2,7 @@ import "./Sidebar.css";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext.jsx";
 import { v1 as uuidv1 } from 'uuid';
+import blackLogo from './assets/blacklogo.png';
 function Sidebar() {
   const {allThreads,setAllThreads,currThreadId,setNewChat,setPrompt,setReply,setCurrThreadId,setPrevChats}=useContext(MyContext);
   const getAllThreads= async()=>{
@@ -72,7 +73,7 @@ function Sidebar() {
         <section className="sidebar" >
            {/*New Chat Button*/}
            <button onClick={createNewChat}>
-            <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo">
+            <img src={blackLogo} alt="gpt logo" className="logo">
             </img>
            <span><i className="fa-solid fa-pen-to-square"></i></span>
            </button>
